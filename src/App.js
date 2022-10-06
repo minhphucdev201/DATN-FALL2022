49576ea320e918ccbed86837dae958a2aaff2639
Minh Phuc <minhphuc201.dev@gmail.com>
2022-10-06 21:00:38 +0700

    update folder

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Footer from "./components/Footers";
 // import './App.css';
 import Header from "./components/Headers";
+import HomePage from "./features/HomePage";
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Footer />
     </div>
   );
---
+++ src/features/HomePage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+HomePage.propTypes = {};
+
+function HomePage(props) {
+  return <div>hi</div>;
+}
+
+export default HomePage;
